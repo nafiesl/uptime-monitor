8eadad740783c06b122fcce3150feae1c94badad
Nafies Luthfi <nafiesl@gmail.com>
2023-07-25 15:35:48 +0800

    feat: Show logs in table

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MonitoringLog;
+
 class MonitoringController extends Controller
 {
     public function timeline()
--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -4,19 +4,23 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Uptime Monitor</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Uptime Monitor</h1>
-    <table class="table table-sm">
-        <tbody>
-            @foreach ($logs as $key => $monitoringLog)
-                <td>{{ $key++ }}</td>
-                <td>{{ $monitoringLog->url }}</td>
-                <td>{{ $monitoringLog->response_time }}</td>
-                <td>{{ $monitoringLog->status_code }}</td>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="container">
+        <h1>Uptime Monitor</h1>
+        <table class="table table-sm">
+            <tbody>
+                @foreach ($logs as $key => $monitoringLog)
+                <tr>
+                    <td>{{ $key++ }}</td>
+                    <td>{{ $monitoringLog->url }}</td>
+                    <td>{{ $monitoringLog->response_time }}</td>
+                    <td>{{ $monitoringLog->status_code }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
