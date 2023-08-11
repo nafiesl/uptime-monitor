fdddc8159d90414bd9c145128fda7d27137135bf
Nafies Luthfi <nafiesl@gmail.com>
2023-08-11 23:30:29 +0800

    fix: Add the missing customer_sites.is_active

--- database/migrations/2023_07_25_070425_create_customer_sites_table.php
+++ database/migrations/2023_07_25_070425_create_customer_sites_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('url');
+            $table->unsignedYinyInteger('is_active')->default(1);
             $table->timestamps();
         });
     }
