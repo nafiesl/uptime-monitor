e324db7b6a6b39c081fdf87a2ce36f6bcafdbf0e
Nafies Luthfi <nafiesl@gmail.com>
2023-11-17 11:39:45 +0800

    feat: Change graph to log indicators

--- resources/views/monitoring/index.blade.php
+++ resources/views/monitoring/index.blade.php
@@ -5,6 +5,7 @@
 <div class="page-header mt-0 mb-4">
     <h1 class="page-title">Uptime Monitor</h1>
 </div>
+<div class="text-end me-3">last 60 minutes</div>
 
 <div class="row mb-4">
     @foreach ($customerSites as $customerSite)
@@ -15,18 +16,35 @@
                         {{ link_to_route(
                             'customer_sites.show',
                             __('app.show'),
-                            [$customerSite, 'time_range' => '6h'],
+                            [$customerSite],
                             ['id' => 'show-customer_site-' . $customerSite->id, 'class' => 'float-end']
                         ) }}
                     @endcan
                     {{ $customerSite->name }}
                 </div>
                 <div class="card-body">
-                    <ul class="pl-1">
+                    {{-- <ul class="pl-1">
                         <li>URL: {{ $customerSite->url }}</li>
-                    </ul>
-                    <hr>
-                    <div id="chart_timeline_{{ $customerSite->id }}"></div>
+                    </ul> --}}
+                    @php
+                        $monitoringLogs = $customerSite->monitoringLogs()
+                            ->whereBetween('created_at', [Carbon::now()->subHours(1), Carbon::now()])
+                            ->get(['response_time', 'created_at']);;
+                    @endphp
+                    <div class="text-center">
+                        @foreach ($monitoringLogs as $monitoringLog)
+                            @php
+                                $bgColor = 'success';
+                                if ($monitoringLog->response_time > 5000) {
+                                    $bgColor = 'warning';
+                                }
+                                if ($monitoringLog->response_time > 10000) {
+                                    $bgColor = 'danger';
+                                }
+                            @endphp
+                            <span class="badge bg-{{ $bgColor }} log_indicator" title="{{ $monitoringLog->created_at }} ({{ number_format($monitoringLog->response_time) }} ms)">&nbsp;</span>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
@@ -34,90 +52,11 @@
 </div>
 @endsection
 
-@push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-<script>
-    @foreach ($customerSites as $customerSite)
-        @php
-            $monitoringLogs = $customerSite->monitoringLogs()
-                ->whereBetween('created_at', [Carbon::now()->subHours(6), Carbon::now()])
-                ->get(['response_time', 'created_at']);
-            $chartData = [];
-            foreach ($monitoringLogs as $monitoringLog) {
-                $chartData[] = ['x' => $monitoringLog->created_at, 'y' => $monitoringLog->response_time];
-            }
-        @endphp
-        var options = {
-            series: [{
-                data: {!! json_encode($chartData) !!}
-            }],
-            chart: {
-                id: 'line-datetime',
-                type: 'line',
-                height: 250,
-                zoom: {
-                    autoScaleYaxis: true
-                }
-            },
-            annotations: {
-                yaxis: [{
-                    y: 5000,
-                    borderColor: 'orange',
-                    label: {
-                        show: true,
-                        text: 'Threshold',
-                        style: {
-                            color: "#fff",
-                            background: 'orange'
-                        }
-                    }
-                }, {
-                    y: 10000,
-                    borderColor: 'red',
-                    label: {
-                        show: true,
-                        text: 'Down',
-                        style: {
-                            color: "#fff",
-                            background: 'red'
-                        }
-                    }
-                }]
-            },
-            dataLabels: {
-                enabled: false
-            },
-            xaxis: {
-                type: 'datetime',
-                min: new Date("{{ Carbon::now()->subHours(6)->format('Y-m-d H:i:s') }}").getTime(),
-                max: new Date("{{ Carbon::now()->format('Y-m-d H:i:s') }}").getTime(),
-                labels: {
-                    datetimeUTC: false,
-                },
-                title: {
-                    text: 'Datetime',
-                },
-            },
-            yaxis: {
-                tickAmount: 6,
-                title: {
-                    text: 'Miliseconds',
-                },
-                max: 12000,
-                min: 0,
-            },
-            stroke: {
-              width: [2]
-            },
-            tooltip: {
-                x: {
-                    format: 'dd MMM yyyy'
-                }
-            },
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart_timeline_{{ $customerSite->id }}"), options);
-        chart.render();
-    @endforeach
-</script>
+@push('styles')
+<style>
+    .log_indicator {
+        padding: 4px 1px;
+        cursor: pointer;
+    }
+</style>
 @endpush
