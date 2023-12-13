0c97a960684181f33627992347733e08296b3efa
Nafies Luthfi <nafiesl@gmail.com>
2023-12-13 11:13:20 +0800

    refactor: Move monitoring logs SQL query into component class

--- app/Livewire/UptimeBadge.php
+++ app/Livewire/UptimeBadge.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Models\CustomerSite;
+use App\Models\MonitoringLog;
 use Livewire\Component;
 
 class UptimeBadge extends Component
@@ -9,11 +11,13 @@ class UptimeBadge extends Component
     public $uptimePoll = 0;
     public $uptimePollState = null;
     public $customerSite;
+    public $monitoringLogs;
 
     public function mount()
     {
+        $customerSite = $this->customerSite;
         if ($this->uptimePoll == 1) {
-            $checkPeriodeInSeconds = $this->customerSite->check_periode * 60;
+            $checkPeriodeInSeconds = $customerSite->check_periode * 60;
             $this->uptimePollState = 'wire:poll.'.($checkPeriodeInSeconds).'s';
         }
     }
@@ -21,5 +25,38 @@ class UptimeBadge extends Component
     public function render()
     {
         return view('livewire.uptime_badge');
+    }
+
+    private function getCustomerSiteMonitoringLogs(CustomerSite $customerSite)
+    {
+        $monitoringLogs = $customerSite->monitoringLogs()
+            ->latest('id')
+            ->take(15)
+            ->get(['response_time', 'created_at'])
+            ->sortKeysDesc()
+            ->map(function ($monitoringLog) use ($customerSite) {
+                $monitoringLog->uptime_badge_bg_color = $this->getUptimeBadgeBgColor($monitoringLog);
+                $monitoringLog->uptime_badge_title = $this->getUptimeBadgeTitle($monitoringLog);
+
+                return $monitoringLog;
+            });
+        return $monitoringLogs;
+    }
+
+    private function getUptimeBadgeBgColor(MonitoringLog $monitoringLog): string
+    {
+        if ($monitoringLog->response_time > 10000) {
+            return 'danger';
+        }
+        if ($monitoringLog->response_time > 5000) {
+            return 'warning';
+        }
+
+        return 'success';
+    }
+
+    private function getUptimeBadgeTitle(MonitoringLog $monitoringLog): string
+    {
+        return $monitoringLog->created_at.' ('.number_format($monitoringLog->response_time).' ms)';
     }
 }
--- resources/views/livewire/uptime_badge.blade.php
+++ resources/views/livewire/uptime_badge.blade.php
@@ -1,21 +1,7 @@
 <div {{ $uptimePollState }}>
-    @php
-        $monitoringLogs = $customerSite->monitoringLogs()
-            ->latest('id')
-            ->take(15)
-            ->get(['response_time', 'created_at'])
-            ->sortKeysDesc();
-    @endphp
-    @foreach ($monitoringLogs as $monitoringLog)
-        @php
-            $bgColor = 'success';
-            if ($monitoringLog->response_time > 5000) {
-                $bgColor = 'warning';
-            }
-            if ($monitoringLog->response_time > 10000) {
-                $bgColor = 'danger';
-            }
-        @endphp
-        <span class="badge bg-{{ $bgColor }} log_indicator" title="{{ $monitoringLog->created_at }} ({{ number_format($monitoringLog->response_time) }} ms)">&nbsp;</span>
+    @foreach ($this->getCustomerSiteMonitoringLogs($customerSite) as $monitoringLog)
+        <span
+            class="badge bg-{{ $monitoringLog->uptime_badge_bg_color }} log_indicator"
+            title="{{ $monitoringLog->uptime_badge_title }}">&nbsp;</span>
     @endforeach
 </div>
