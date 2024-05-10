9e918b346eff8f1105b119a824312f621265ea6d
Azrul Haifan <ahaifan@gmail.com>
2024-05-10 10:29:59 +0700

    Adding user prefs table to use later ++ threshold value on this table will be used as default threshold value for new customer site

---
+++ database/migrations/2024_05_10_030731_create_user_preferences_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_preferences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('warning_threshold');
+            $table->unsignedInteger('down_threshold');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_preferences');
+    }
+};
