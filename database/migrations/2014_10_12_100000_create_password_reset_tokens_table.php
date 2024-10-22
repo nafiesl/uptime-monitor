981c56fdaf09367e0be9cbfa3dab1dde2956cb02
keziaeci <keziaeci2806@gmail.com>
2024-10-22 13:08:09 +0700

    fix migration

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email',100)->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
